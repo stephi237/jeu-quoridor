36e948f4f9341d63563fd7f1025d2a4c58050088
stephi237 <tresorsteph.mukam@gmail.com>
2019-11-15 17:36:59 -0500

    update

--- api.py
+++ api.py
@@ -1,10 +1,135 @@
-import partie1
+import argparse
+import requests
 import json
 
-
-def lister_parties(idul):
-    partie1.lister_parties(idul)
-
-def débuter_partie(idul):
-    partie1.débuter_partie(idul)
+#--------------------------------------Fonctions obligatoires du module api -----------------------------------------------
     
+def lister_parties(args):
+    #liste les 20 dernières parties du joueur 
+    url_base = 'https://python.gel.ulaval.ca/quoridor/api/'
+    payload = {'idul': args.idul}
+    rep = requests.get(url_base+'lister/' , params=payload)
+    if rep.status_code == 200:
+        rep = rep.json()
+        return rep['parties']
+    else:
+        print(f"Le GET sur {url_base+'lister'} a produit le code d'erreur {rep.status_code}.")
+        raise RuntimeError
+
+
+def débuter_partie(args):
+    #renvoit les informations d'un début de partie 
+    url_base = 'https://python.gel.ulaval.ca/quoridor/api/'
+    rep = requests.post(url_base+'débuter/', data={'idul': args.idul})
+    rep = rep.json()
+    if 'id' in rep and 'état' in rep:
+        
+        return (rep['id'], rep['état'])
+    else:
+        print(rep['message'])
+        raise RuntimeError
+
+
+def jouer_coup(id_partie, type_coup ,position):
+    url_base = 'https://python.gel.ulaval.ca/quoridor/api/'
+    payload = { 'id': id_partie, 'type': type_coup, 'pos':position }        
+    rep = requests.post(url_base+'jouer/', data=payload)
+    rep = rep.json()       
+    if 'gagnant' in rep:
+        print(rep["gagnant"])
+        raise StopIteration
+    elif 'message' in rep:
+        print(rep['message'])
+        raise RuntimeError
+    return rep['état']
+
+
+#----------------------------------------- Fonctions auxilliaires pour afficher le damier -------------------------------------------------
+
+def dechiffrage_du_json(etat_du_jeu):
+    # convertit le langage json en objet utilisable
+    if(type(etat_du_jeu) == str):
+        data = json.loads(etat_du_jeu)
+    else:
+        data = etat_du_jeu
+    joueurs = data["joueurs"]
+    j1 = joueurs[0]["pos"]
+    j2 = joueurs[1]["pos"]
+    murs = data["murs"]
+    mh = murs["horizontaux"]
+    mv = murs["verticaux"]
+    info = [j1, j2, mh,mv]
+    return info 
+
+def incorporation_element_matrice(matrice, etat_du_jeu ):
+    # insere les elements recupérés de l'état du jeu pour renvoyer le damier correspondant
+    [j1, j2, mh,mv] = dechiffrage_du_json(etat_du_jeu) 
+    [j1,j2] = traduction_position_joueur([j1,j2])
+    mh = traduction_position_murs_horizontaux(mh)
+    mv = traduction_position_murs_verticaux(mv)
+    for i in mh:
+        for j in range(i[1], i[1]+7):
+            matrice[i[0]][j] = '-'
+    for i in mv:
+        for j in range(i[0], i[0]+3):
+            matrice[j][i[1]] = '|'
+    matrice[j1[0]][j1[1]] = '1'
+    matrice[j2[0]][j2[1]] = '2'
+    return matrice
+
+
+def traduction_position_joueur(joueurs ):
+    # convertit les coordonnés visuels en coordonnées actuels 
+    for i in joueurs:
+        i[0], i[1] = 2*(10-i[1]), i[0]*4
+    return joueurs
+
+def traduction_position_murs_horizontaux(mh):
+    # convertit les coordonnés visuels en coordonnées actuels 
+    for i in mh:
+        i[0] , i[1] = 2*(10-i[1])+1, 4*i[0]-1
+    return mh
+
+def traduction_position_murs_verticaux(mv):
+    # convertit les coordonnés visuels en coordonnées actuels 
+    for i in mv:
+        i[0] , i[1]= 2*(10-i[1])-2, 4*i[0]-2
+    return mv
+
+
+
+def creation_matrice():
+    # création du plateau de jeu sans joueurs ni murs
+    ligne = 21
+    colone = 39
+    numerotation = 9
+    matrice = []
+    matrice.append(['Légende: 1=idul, 2=automate'])
+    matrice.append(['   -----------------------------------'])
+    for i in range(2, ligne-2):
+        matrice.append([' ']*39)
+    matrice.append(['--|-----------------------------------'])
+    matrice.append(['  | 1   2   3   4   5   6   7   8   9'])
+    for i in range(2, ligne-2):
+        matrice[i][2] = '|'
+        matrice[i][38] = '|'
+        if (i%2) == 0 :
+            matrice[i][0] = str(numerotation)
+            numerotation -= 1
+            for j in range(1, 10):
+                matrice[i][4*j] = '.'
+    return matrice
+
+
+def creation_damier(etat_du_jeu):
+    # crée la matrice grâce à l'etat du jeu 
+    matrice = creation_matrice()
+    incorporation_element_matrice(matrice, etat_du_jeu)
+    return matrice
+
+
+def afficher_matrice(matrice):
+    # affiche la matrice sous forme de damier
+    for indice, mat1 in enumerate(matrice):
+        matrice[indice] = ''.join(mat1)
+    return'\n'.join(matrice) 
--- main.py
+++ main.py
@@ -1,15 +1,34 @@
+import api
 import argparse
-import json
-import partie1
-
 
 def analyser_commande():
-    partie1.analyser_commande()
+    # analyseur de ligne de commande
+    parser = argparse.ArgumentParser(description = 'Jeu Quoridor - phase 1')
+    parser.add_argument('idul', help= 'IDUL du joueur.', type= str )
+    parser.add_argument('-l', '--lister',action='store_true' , help= 'Lister les identifiants de vos 20 dernières parties.')
+    return parser.parse_args()
+
 
 def afficher_damier_ascii(etat_du_jeu):
-    matrice = partie1.creation_damier(etat_du_jeu)
-    matrice = partie1.afficher_matrice(matrice)
+    matrice = api.creation_damier(etat_du_jeu)
+    matrice = api.afficher_matrice(matrice)
     print(matrice)
 
-
+# lancement d'une partie
+args = analyser_commande()
+if args.lister == True:
+    print(api.lister_parties(args))
+(identifiant , etat) = api.débuter_partie(args)
+start = True
+while start == True :
+    afficher_damier_ascii(etat)
+    print('\t Entre le type de coup que tu veux effectuer -- :')
+    print("\t 'D' pour déplacer le jeton \n\t 'MH' pour placer un mur horizontal \n\t ou 'MV' pour placer un mur vertical ")
+    type_coup = input()
+    position = []
+    print('Entre la position (x, y) correspondante')
+    position.append(input ('Entre la position x correspondante'))
+    position.append(input ('Entre la position y correspondante'))
+    etat = api.jouer_coup(identifiant, type_coup ,position)
+   
 
